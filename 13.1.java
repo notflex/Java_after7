ac27c40ddaed5777fa5fd8c306fa08409e040f74
notflex <54347210+notflex@users.noreply.github.com>
2022-10-21 15:13:56 +0300

    Create 13.1.java

---
+++ 13.1.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) throws Exception {
+            try(SampleClass sc1 = new SampleClass()){
+                sc1.do_smth();
+            }
+        }
+    }
